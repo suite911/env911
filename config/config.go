9874ef6dbd8b54b4b63e29dc4afceaa790848342
Amy <amyadzuki@gmail.com>
2018-08-12 23:56:36 +0000

    Update config.go

--- config/config.go
+++ config/config.go
@@ -6,22 +6,22 @@ import (
 
 // Config describes the app configuration.
 type Config struct {
-	app *app.Apper
+	app  app.Apper
 	map_ map[string]interface{}
 }
 
 // New creates a new Config.
-func New(app *app.Apper, args ...interface{}) *Config {
+func New(app app.Apper, args ...interface{}) *Config {
 	return new(Config).Init(app, args...)
 }
 
 // App gets the app being configured.
-func (config *Config) App() *app.Apper {
+func (config *Config) App() app.Apper {
 	return config.app
 }
 
 // Init initializes a Config.
-func (config *Config) Init(app *app.Apper, args ...interface{}) *Config {
+func (config *Config) Init(app app.Apper, args ...interface{}) *Config {
 	config.app = app
 	config.map_ = make(map[string]interface{})
 	return config
