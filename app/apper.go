382228dec3cf5309cd2782de787fb2fdda365b85
Amy <amyadzuki@gmail.com>
2018-08-12 23:52:21 +0000

    Update apper.go

--- app/apper.go
+++ app/apper.go
@@ -2,16 +2,41 @@ package app
 
 // Apper is the interface for an app.
 type Apper interface {
-	Cache()       string
-	Config()      string
-	Data()        string
-	Desktop()     string
-	Documents()   string
-	Downloads()   string
-	Home()        string
-	Name()        string
-	Path()        string
-	Pictures()    string
+
+	// Cache gets the full path to the local app cache directory.
+	Cache() string
+
+	// Config gets the full path to the local app config directory.
+	Config() string
+
+	// Data gets the full path to the local app data directory.
+	Data() string
+
+	// Desktop gets the full path to the current user's Desktop directory.
+	Desktop() string
+
+	// Documents gets the full path to the current user's Documents directory.
+	Documents() string
+
+	// Downloads gets the full path to the current user's Downloads directory.
+	Downloads() string
+
+	// Home gets the full path to the current user's Home directory.
+	Home() string
+
+	// Name gets the app name.
+	Name() string
+
+	// Path gets the app path.
+	Path() string
+
+	// Pictures gets the full path to the current user's Pictures directory.
+	Pictures() string
+
+	// Screenshots gets the full path to the current user's Screenshots directory.
 	Screenshots() string
-	Vendor()      string
+
+	// Vendor gets the app vendor.
+	Vendor() string
+
 }
