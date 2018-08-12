32e9b419148972af11817025192d9636c8dc9c5e
Amy <amyadzuki@gmail.com>
2018-08-12 23:56:51 +0000

    Update configge.go

--- config/configge.go
+++ config/configge.go
@@ -4,7 +4,7 @@ package config
 type Configger interface {
 
 	// App gets the app being configured.
-	App() *app.Apper
+	App() app.Apper
 
 	// Map gets the map of keys to configured values.
 	Map() map[string]interface{}
