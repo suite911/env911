209e9791b5fde8ddf1c1068be0792587df13dc42
Amy <amyadzuki@gmail.com>
2018-08-12 23:34:31 +0000

    Create apper.go

---
+++ app/apper.go
@@ -0,0 +1,16 @@
+package app
+
+type Apper interface {
+	Cache()       string
+	Config()      string
+	Data()        string
+	Desktop()     string
+	Documents()   string
+	Downloads()   string
+	Home()        string
+	Name()        string
+	Path()        string
+	Pictures()    string
+	Screenshots() string
+	Vendor()      string
+}
