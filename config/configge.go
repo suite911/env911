4783cbfc11dd5dc1c945a9ea069d8e1eac0d9ba5
Amy <amyadzuki@gmail.com>
2018-08-13 08:36:58 +0000

    Update configge.go

--- config/configge.go
+++ config/configge.go
@@ -22,6 +22,18 @@ type Configger interface {
 
 }
 
+func (config Configger) Bool(name string, value bool, usage string) {
+}
+
+func (config Configger) BoolP(name, shorthand string, value bool, usage string) {
+}
+
+func (config Configger) BoolVar(p *bool, name string, value bool, usage string) {
+}
+
+func (config Configger) BoolVarP(p *bool, name, shorthand string, value bool, usage string) {
+}
+
 // Get gets the value associated with key from the configuration sources
 func (config Configger) Get(key string) interface{} {
 	if v, ok := config.Env(); ok {
@@ -36,9 +48,33 @@ func (config Configger) Get(key string) interface{} {
 	return nil
 }
 
+func (config Configger) Int(name string, value int, usage string) {
+}
+
+func (config Configger) IntP(name, shorthand string, value int, usage string) {
+}
+
+func (config Configger) IntVar(p *int, name string, value int, usage string) {
+}
+
+func (config Configger) IntVarP(p *int, name, shorthand string, value int, usage string) {
+}
+
 // Load loads mappings from the several sources.
 func (config Configger) Load(onFail ...onfail.OnFail) error {
 	config.LoadConfigFile(config.System(), config.App().SystemConfigFile(), ...onFail)
 	config.LoadConfigFile(config.Local(), config.App().LocalConfigFile(), ...onFail)
 	config.LoadEnv(config.Env(), ...onFail)
+}
+
+func (config Configger) String(name, value, usage string) {
+}
+
+func (config Configger) StringP(name, shorthand, value string, usage string) {
+}
+
+func (config Configger) StringVar(p *string, name, value, usage string) {
+}
+
+func (config Configger) StringVarP(p *string, name, shorthand, value string, usage string) {
 }
