690ae80d3a29ab2aa72967954b5e2ae856bef04b
Amy <amyadzuki@gmail.com>
2018-08-14 10:03:13 +0000

    Update flagger.go

--- config/flagger.go
+++ config/flagger.go
@@ -2,22 +2,22 @@ package config
 
 // Flagger is the interface for a flag replacement library.
 type Flagger interface {
-	Bool(string, bool, string)
-	BoolP(string, string, bool, string)
+	Bool(string, bool, string) *bool
+	BoolP(string, string, bool, string) *bool
 	BoolVar(*bool, string, bool, string)
 	BoolVarP(*bool, string, string, bool, string)
-	Count(string, int, string)
-	CountP(string, string, int, string)
+	Count(string, int, string) *int
+	CountP(string, string, int, string) *int
 	CountVar(*int, string, int, string)
 	CountVarP(*int, string, string, int, string)
 	CountWorks() bool
-	Int(string, int, string)
-	IntP(string, string, int, string)
+	Int(string, int, string) *int
+	IntP(string, string, int, string) *int
 	IntVar(*int, string, int, string)
 	IntVarP(*int, string, string, int, string)
 	Parse([]string) error
-	String(string, string, string)
-	StringP(string, string, string, string)
+	String(string, string, string) *string
+	StringP(string, string, string, string) *string
 	StringVar(*string, string, string, string)
 	StringVarP(*string, string, string, string, string)
 }
