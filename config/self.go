93955835e81fdd231cb8ac8654780b5fd41627a4
Amy <amyadzuki@gmail.com>
2018-08-15 15:28:39 +0000

    Update self.go

--- config/self.go
+++ config/self.go
@@ -85,7 +85,7 @@ func Env() map[string]interface{} {
 }
 
 // FlagSet gets the flag set used for configuration.
-func FlagSet() *flagger.Flagger {
+func FlagSet() flagger.Flagger {
 	return self.FlagSet()
 }
 
