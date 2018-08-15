9319cc73143371c43d562df65762f8067a0497ab
Amy <amyadzuki@gmail.com>
2018-08-15 01:10:05 +0000

    Update pflag.go

--- config/pflag.go
+++ config/pflag.go
@@ -1,9 +1,9 @@
 package config
 
-import "github.com/ogier/pflag"
+import flag "github.com/ogier/pflag"
 
 type PFlag struct {
-	*pflag.FlagSet
+	*flag.FlagSet
 }
 
 func (pflag PFlag) Count(name string, by int, usage string) *int {
@@ -22,8 +22,35 @@ func (pflag PFlag) CountVarP(p *int, name, shorthand string, by int, usage strin
 	panic("github.com/ogier/pflag does not support the Count* functions")
 }
 
+// TODO: make my own CC0-1.0-licensed POSIX-compliant flag replacement library that does
+
 func (pflag PFlag) CountWorks() bool {
 	return false
 }
 
-// TODO: make my own CC0-1.0-licensed POSIX-compliant flag replacement library that does
+func (pflag PFlag) PVersionsWork() bool {
+	return true
+}
+
+func (pflag PFlag) SetUsageFooter(text string) {
+	flag.Usage = usage
+	pflag.UsageFooter = text
+}
+
+func (pflag PFlag) SetUsageHeader(text string) {
+	flag.Usage = usage
+	pflag.UsageHeader = text
+}
+
+var UsageFooter, UsageHeader string
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:", os.Args[0])
+	if len(UsageHeader) > 0 {
+		fmt.Fprintf(os.Stderr, UsageHeader)
+	}
+	flag.PrintDefaults()
+	if len(UsageFooter) > 0 {
+		fmt.Fprintf(os.Stderr, UsageFooter)
+	}
+}
