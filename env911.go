682fd2913434cc5138bc2397478e86f34df680d1
Amy <amyadzuki@gmail.com>
2018-08-15 00:33:57 +0000

    Update env911.go

--- env911.go
+++ env911.go
@@ -5,9 +5,15 @@ import (
 	"github.com/amy911/env911/config"
 )
 
+// InitAll initializes every part of env911 at once.
 func InitAll(prefix string, flagSet config.Flagger, path ...string) {
 	a := app.New(path...)
 	app.Init(a)
 	c := config.New(prefix, flagSet, a)
 	config.Init(c)
+}
+
+// IsInitAll reports whether or not every part of env911 has been initialized.
+func IsInitAll() bool {
+	return app.IsInit() && config.IsInit()
 }
